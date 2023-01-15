f7bdb85e3a6b237f5b89aca82323154693c7ee6c
Shakil <shakil_miah@hotmail.com>
2023-01-15 12:50:53 -0500

    set up file structure
    created the file structure

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Article} from './article/Article';
+export {default as Brand} from './brandCTA/Brand';
+export {default as CTA} from './ctaFeature/CTA';
+export {default as Feature} from './feature/Feature';
+export {default as Navbar} from './navbar/Navbar';
---
+++ src/containers/index.js
@@ -0,0 +1,6 @@
+export {default as Blog} from './blog/Blog';
+export {default as Features} from './whatGPT3/Features';Footer
+export {default as Footer} from './footerHeader/Footer';
+export {default as Header} from './headerPossibility/Header';
+export {default as Possibility} from './posibilityBlog/Possibility';
+export {default as WhatGPT3} from './whatGPT3/WhatGPT3';
